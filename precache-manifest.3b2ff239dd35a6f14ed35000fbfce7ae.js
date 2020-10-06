596b34069bd60d283ae089a7304e9eee203dcacf
ZychGrzegorz <grzegorzzych95@gmail.com>
2020-10-06 18:57:41 +0200

    Updates

---
+++ precache-manifest.3b2ff239dd35a6f14ed35000fbfce7ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8751289d68d4b357e3c64dc440fd20",
+    "url": "/todo-ts-react/index.html"
+  },
+  {
+    "revision": "0ad2e36c3165a493501e",
+    "url": "/todo-ts-react/static/css/main.a7fe800d.chunk.css"
+  },
+  {
+    "revision": "e12a37ae4d769fb25f65",
+    "url": "/todo-ts-react/static/js/2.9c902749.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-ts-react/static/js/2.9c902749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad2e36c3165a493501e",
+    "url": "/todo-ts-react/static/js/main.513f9c55.chunk.js"
+  },
+  {
+    "revision": "1df87a7f89472ea6a781",
+    "url": "/todo-ts-react/static/js/runtime-main.9a97e0aa.js"
+  }
+]);
